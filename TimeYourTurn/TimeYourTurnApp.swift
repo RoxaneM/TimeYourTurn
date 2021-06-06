3663311caee8e9ef415a358ef13c51edf4ecf4e2
Roxane Gud <roxane654@gmail.com>
2021-06-06 19:37:33 +0300

    Created Xcode project

---
+++ TimeYourTurn/TimeYourTurnApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeYourTurnApp.swift
+//  TimeYourTurn
+//
+//  Created by Roxane Gud on 6/6/21.
+//
+
+import SwiftUI
+
+@main
+struct TimeYourTurnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
